8529f78b5f882e92bff8e143d9354d2aeba7e645
Ion Reguera <ion@stagelab.net>
2021-04-08 21:14:54 +0200

    Return True or False and errors in self.errors

--- CuemsDeploy.py
+++ CuemsDeploy.py
@@ -18,9 +18,11 @@ class CuemsDeploy():
             self.library_path = library_path
 
         if not log_file:
-            self.log_file = './cuems_rsync.log'
+            self.log_file = '/tmp/cuems_rsync.log'
         else:
             self.log_file = log_file
+
+        self.errors = None
 
         
 
@@ -32,6 +34,7 @@ class CuemsDeploy():
         try:
             result = subprocess.run(['rsync', '-rq', '--stats', f'--files-from={path}', f'--log-file={self.log_file}', self.address, self.library_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=dict(os.environ, RSYNC_PASSWORD="f48t5eL2kLHw2Wfw"))
             result.check_returncode()
+            self.errors = None
             return True
         except subprocess.CalledProcessError as e:
             #print('exit code: {}'.format(e.returncode))
@@ -43,6 +46,6 @@ class CuemsDeploy():
             #convert lines to list and remove last line (final error menssage)
             errors_list = errors_string.splitlines()
             errors_list.pop()
-
-            return errors_list
+            self.errors = errors_list
+            return False
 
